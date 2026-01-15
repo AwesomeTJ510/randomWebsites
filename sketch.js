34b4e8d968bb82042d6f4eaec8a1c3d59fee3da9
Tyler Cunningham <awesometj510@gmail.com>
2026-01-15 11:32:41 -0700

    Added Scripts

---
+++ sketch.js
@@ -0,0 +1,61 @@
+let startSize = 100
+let circleCount = 500
+
+let targetX = 0
+let targetY = 0
+
+let posX = 0
+let posY = 0
+
+let color1 = [255, 0, 0]
+let color2 = [0, 255, 0]
+
+let fullscreenChange = false;
+
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  if (fullscreenChange == 0) {
+    if (fullscreen()) {
+      resizeCanvas(windowWidth, windowHeight)
+    } else {
+      resizeCanvas(600, 600)
+    }
+  }
+  
+  strokeWeight(0)
+  
+  background(0);
+  
+  targetX = 0
+  targetY = 0
+  
+  if (mouseIsPressed) {
+    targetX = mouseX - (width / 2)
+    targetY = mouseY - (height / 2)
+  }
+  
+  posX += (targetX - posX) / 10
+  posY += (targetY - posY) / 10
+  
+  for (let i = 0; i < circleCount; i++) {
+    fill(color2[0] + (((color1[0] - color2[0]) / circleCount) * i), color2[1] + (((color1[1] - color2[1]) / circleCount) * i), color2[2] + (((color1[2] - color2[2]) / circleCount) * i))
+    
+    circle(((posX) / circleCount) * i + (width / 2), ((posY) / circleCount) * i + (height / 2), startSize * (1 / Math.pow(2, (3 / circleCount) * (circleCount - (i + 1)))))
+  }
+  
+  fullscreenChange--
+}
+
+function keyPressed() {
+  if (key == "f") {
+    if (fullscreen()) {
+      fullscreen(false)
+    } else {
+      fullscreen(true)
+    }
+    fullscreenChange = 2
+  }
+}
